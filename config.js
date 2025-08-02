2389d4a94c459d65cf90b8d08a5f4574af65debc
Shorya Agarwal <60816807+AHopefulIdiot@users.noreply.github.com>
2025-08-02 13:54:39 +0530

    Update config.js with BITS Goa coords (fuck I'm gonna cry)

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '27.592',
-	defaultLongitude: '76.609',
+	defaultLatitude: '15.392',
+	defaultLongitude: '73.883',
 
 	// Autochange
 	autoChangeTheme: false,
